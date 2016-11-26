3c9bbba9f0f05c6633658c38bca466f7b2e11f55
michaelknigge <michael.knigge@gmx.de>
2016-11-26 20:25:05 +0100

    Added a  JUnit test

--- pclbox/src/test/java/mk/pclbox/PclParserTest.java
+++ pclbox/src/test/java/mk/pclbox/PclParserTest.java
@@ -3,6 +3,7 @@ package mk.pclbox;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 import junit.framework.TestCase;
 
@@ -20,5 +21,31 @@ public final class PclParserTest extends TestCase {
         new PclParser(testFile).close();
         new PclParser(new FileInputStream(testFile)).close();
         new PclParser(new ByteArrayInputStream(new byte[0])).close();
+        
+        new PclParser(new PclInputStream() {
+            
+            @Override
+            public void seek(long offset) throws IOException {
+            }
+            
+            @Override
+            public int read(byte[] b, int off, int len) throws IOException {
+                return -1;
+            }
+            
+            @Override
+            public int read(byte[] b) throws IOException {
+                return -1;
+            }
+            
+            @Override
+            public int read() throws IOException {
+                return -1;
+            }
+            
+            @Override
+            public void close() throws IOException {
+            }
+        }).close();
     }
 }
